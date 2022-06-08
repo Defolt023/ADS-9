d0e70e80a093b1868b10586a1c470fa51fde4014
DmitriyBarinov <95102030+Defolt023@users.noreply.github.com>
2022-06-08 21:46:49 +0300

    Create alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,29 +6,29 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
- std::ifstream file(filename);
-  std::string wwordl;
-  BST<std::string> deeer;
-  std::string leier;
+  std::ifstream file(filename);
+  BST<std::string> derevo;
+  std::string lier;
+  std::string slovo;
   while (!file.eof()) {
-  file >> wwordl;
-  int h = 0;
-  while (h < wwordl.length()) {
-  int z = 0;
-  while ((wwordl[h] >= 'a' && wwordl[h] <= 'z' || wwordl[h] >= 'A' && wwordl[h] <= 'Z') && h < wwordl.length()) {
-  if (wwordl[h] >= 'A' && wwordl[h] <= 'Z')
-  wwordl[h] += 32;
-  leier += wwordl[h];
-  h++;
-  z++;
+  file >> slovo;
+  int i = 0;
+  while (i < slovo.length()) {
+  int j = 0;
+  while ((slovo[i] >= 'a' && slovo[i] <= 'z' || slovo[i] >= 'A' && slovo[i] <= 'Z') && i < slovo.length()) {
+  if (slovo[i] >= 'A' && slovo[i] <= 'Z')
+  slovo[i] += 32;
+  lier += slovo[i];
+  i++;
+  j++;
   }
-  if (z != 0) {
-  deeer.add(leier);
-  leier = "";
+  if (j != 0) {
+  derevo.add(lier);
+  lier = "";
   } else {
-  h++;
+  i++;
+  }   
   }
   }
-  }
-  return deeer;
+  return derevo;
 }
